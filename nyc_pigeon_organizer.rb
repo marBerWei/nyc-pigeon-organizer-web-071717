818f779036f15279e592f90e22553250c6c6d67f
Martina Weidenbaum <martina.weidenbaum@gmail.com>
2017-07-13 22:39:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category, hash|
+    hash.each do |specific, array|
+      array.each do |pigeon|
+        if !pigeon_list.include?(pigeon)
+          pigeon_list[pigeon] = {}
+        end
+        if category != :color
+          pigeon_list[pigeon][category] = [specific.to_s]
+        elsif !pigeon_list[pigeon][category]
+          pigeon_list[pigeon][category] = [specific.to_s]
+        else
+          pigeon_list[pigeon][category] << specific.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
